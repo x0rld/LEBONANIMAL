fa4138f6e093be93d50049ca15dc2b9e5ce29ecb
LlorensArthur <arthurllorens@hotmail.fr>
2022-02-15 14:46:07 +0100

    Partie admin : Liste des annonces à approuver Partie admin : Liste des annonces supprimés Partie admin : Liste des utilisateurs bannis

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDistributedMemoryCache();
 
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.IdleTimeout = TimeSpan.FromHours(10);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
